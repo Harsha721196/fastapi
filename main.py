41a7d75011d9432f08cf9073fe569c4cb03148ca
harsh <harshanaga00@gmail.com>
2026-06-13 12:44:25 +0530

    adding my third commit

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ posts :list[dict] = [
         "group" : "BlackPink",
         "leader" : "NO-leader"
     },
+    {
+        "name" :"joy",
+        "group" : "Red Velvet",
+        "leader" : "Irene"
+    },
 ]
 @app.get("/home",response_class = HTMLResponse)  # type :ignore
 def home():
@@ -27,6 +32,13 @@ def home():
     <p>{posts[1]['leader']}</p>
     
     <h2>{posts[0]['name']} belongs to, {posts[0]['group']} and the leader is {posts[0]['leader']}</h2>
+    
+    <h2>{posts[1]['name']} belongs to, {posts[1]['group']}</h2>    
+    <pre>{posts[0]}</pre>
+    <pre>{posts[1]}</pre>
+    
+    <h3>Total posts: {len(posts)}</h3>
+    
     """
 
 @app.get("/api/posts")
@@ -34,4 +46,15 @@ def home():
 def get_posts():
     return posts
 print(posts)
+
+@app.get("/leaders",response_class = HTMLResponse)
+
+def get_leaders():
+    return f'''
+    <h1> {posts[0]['leader']} is leader of {posts[0]['group']}</h1>
+    <br>
+    <h1> {posts[1]['group']} has no leader </h1>
+    <br>
+    <h1>{posts[2]['leader']} is leader of {posts[2]['group']}</h1>
+    '''
 
