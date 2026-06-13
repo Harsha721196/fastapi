f5249809df9bf56d1c78752b154e0f738baa0240
harsh <harshanaga00@gmail.com>
2026-06-13 12:20:43 +0530

    adding my second commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI,Request
-from fastapi.responses import HTML Response
-
+from fastapi import FastAPI # type : ignore
+from fastapi.responses import HTMLResponse # type : ignore
 
 
 app = FastAPI()
@@ -15,9 +14,24 @@ posts :list[dict] = [
         "name" : "Lisa",
         "group" : "BlackPink",
         "leader" : "NO-leader"
-    }
+    },
 ]
+@app.get("/home",response_class = HTMLResponse)  # type :ignore
+def home():
+    return f"""
+    <h1>{posts[0]['name']}</h1>
+    <p>Group: {posts[0]['group']}</p>
+    <p>Leader: {posts[0]['leader']}</p>
+    <h1>{posts[1]['name']}</h1>
+    <p>Group: {posts[1]['group']}</p>
+    <p>{posts[1]['leader']}</p>
+    
+    <h2>{posts[0]['name']} belongs to, {posts[0]['group']} and the leader is {posts[0]['leader']}</h2>
+    """
+
 @app.get("/api/posts")
-@app.get("/response_class= HTMLResponse")
+
 def get_posts():
     return posts
+print(posts)
+
